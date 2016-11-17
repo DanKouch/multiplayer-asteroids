25d3c2c61ebed7d30d4af2cac69f3dcd61aea16e
Daniel Kouchekinia <daniel@aris.local>
2016-11-16 23:15:26 -0800

    Implemented socket communication between client and server. Implemented a rudementary session system.

--- app.js
+++ app.js
@@ -1,25 +1,120 @@
-var express = require('express');
-var app = express();
+// Config
+const COLORS = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple"]
+const MAX_PLAYERS_PER_SESSION = 8;
+
+// Imports
+var express = require('express')
+var shortId = require('shortid')
+var escape = require('escape-html')
+var profanityCensor = require('profanity-censor')
+var app = express()
 
 // Webserver Connections
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000)
 
 var server = app.listen(app.get('port'), function () {
-  console.log('Web server started on port ' + app.get('port'));
-});
+  console.log('HTTP server running on port ' + app.get('port'))
+})
 
-var io = require('socket.io').listen(server);
+var io = require('socket.io').listen(server)
 
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+  res.sendFile(__dirname + '/index.html')
+})
 
 // Websocket Connections
 io.on('connection', function(socket){
-  console.log('User connected');
-  socket.on('disconnect', function(){
-    console.log('User disconnected');
-  });
-});
+  socket.on('username', function(unsafeUsername){
+    var safeUsername = profanityCensor.filter(escape(unsafeUsername))
+    if(usernameOk(safeUsername)){
+      socket.playerInfo = {username:safeUsername}
+      getSessionToJoin(socket).addClient(socket)
+      socket.on('disconnect', function(){
+        console.log("Client '" + safeUsername + "' disconnected")
+      })
+    }else{
+      socket.emit("err", "Bad username")
+    }
+  })
+})
+
+function usernameOk(username){
+  return (username.trim() !== "" && username !== "null")
+}
+
+function getSessionToJoin(socket){
+  for(var i = 0; i < sessions.length; i++){
+    if(sessions[i].clients.length < MAX_PLAYERS_PER_SESSION){
+      return sessions[i]
+    }
+  }
+  return sessions[sessions.push(new session)-1]
+}
+
+// Set up sessions
+function session(){
+  this.id = shortId.generate()
+  this.clients = []
+  this.tick = setInterval(this.sendPackets.bind(this), 10)
+
+  console.log("New session created with id " + this.id)
+}
+
+session.prototype.addClient = function(socket){
+  var index = this.clients.push(socket)
+  socket.playerInfo.color = COLORS[index-1]
+  socket.playerInfo.id = shortId.generate()
+
+  console.log("User " + socket.playerInfo.username + " (" + socket.playerInfo.id + ") connected to session " + this.id)
+
+
+
+  // Gather client controls
+  var sessionID = this.id
+
+  socket.on('key press event', function(e){
+    shortId.generate()
+    console.log("User " + socket.playerInfo.username + " (" + socket.playerInfo.id + ") on session " + sessionID + " has " + (e.pressed ? "pressed" : "released") + " key " + e.key)
+  })
+
+  socket.on('mouse press event', function(e){
+    console.log("User " + socket.playerInfo.username + " (" + socket.playerInfo.id + ") on session " + sessionID + " has " + (e.pressed ? "pressed" : "released") + " their mouse")
+  })
+}
+
+// Session object declaration
+session.prototype.sendPackets = function(){
+  for(var i = 0; i < this.clients.length; i++){
+
+    var personalPlayerPacket = []
+    for(var x = 0; x < this.clients.length; x++){
+      if(this.clients[i].playerInfo.id !== this.clients[x].playerInfo.id){
+        personalPlayerPacket.push({
+          username: this.clients[x].playerInfo.username,
+          color: this.clients[x].playerInfo.color,
+          id: this.clients[x].playerInfo.id,
+          x: 0,
+          y: 0
+        })
+      }
+    }
+
+    this.clients[i].emit("server packet", {
+        players: personalPlayerPacket,
+        sessionID: this.id,
+        you: {
+          username: this.clients[i].playerInfo.username,
+          color: this.clients[i].playerInfo.color,
+          id: this.clients[i].playerInfo.id,
+          x: 0,
+          y: 0,
+          health: 0
+        }
+    })
+  }
+}
+
+
+var sessions = [new session()]
--- public/game.js
+++ public/game.js
@@ -1,26 +1,72 @@
 $(function(){
-
+  
   // Set up view
-  var canvas = document.getElementById("gameCanvas");
-  var g = canvas.getContext("2d");
+  var canvas = document.getElementById("gameCanvas")
+  var g = canvas.getContext("2d")
 
-  document.body.style.overflow = "hidden";
-	document.body.style.margin = "0px 0px 0px 0px";
+  document.body.style.overflow = "hidden"
+	document.body.style.margin = "0px 0px 0px 0px"
 
-  canvas.height = window.innerHeight;
-  canvas.width =  window.innerWidth;
+  canvas.height = window.innerHeight
+  canvas.width =  window.innerWidth
 
   window.onresize = function(){
-    canvas.height = window.innerHeight;
-    canvas.width =  window.innerWidth;
-    gameTick();
-  };
+    canvas.height = window.innerHeight
+    canvas.width =  window.innerWidth
+    gameTick()
+  }
 
-  var socket = io();
+  // Manage connection to server
+  var socket = io()
+  socket.emit("username", window.prompt("Please enter a username", "Daniel"))
 
-  var gameLoop = setInterval(gameTick, 10);
+  socket.on('err', function(errorMessage){
+    alert("Error: " + errorMessage)
+  })
+
+  socket.on('server packet', function(publicPacket){
+    console.log(publicPacket)
+  })
+
+  // Handle user input and send to server
+  var keysPressed = []
+
+	document.addEventListener("keydown", function(e){
+	  if(keysPressed.indexOf(String.fromCharCode(e.keyCode)) === -1){
+	  	keysPressed.push(String.fromCharCode(e.keyCode))
+	    socket.emit("key press event", {
+        key: String.fromCharCode(e.keyCode),
+        pressed: true
+      })
+	  }
+	})
+
+	document.addEventListener("keyup", function(e){
+	  if(keysPressed.indexOf(String.fromCharCode(e.keyCode)) !== -1){
+	  	keysPressed.splice(keysPressed.indexOf(String.fromCharCode(e.keyCode)))
+      socket.emit("key press event", {
+        key: String.fromCharCode(e.keyCode),
+        pressed: false
+      })
+	  }
+	})
+
+	document.addEventListener("mousedown", function(e){
+    socket.emit("mouse press event", {
+      pressed: true
+    })
+	})
+
+	document.addEventListener("mouseup", function(e){
+    socket.emit("mouse press event", {
+      pressed: false
+    })
+	})
+
+  // Handle overall game tick
+  var gameLoop = setInterval(gameTick, 10)
 
   function gameTick(){
-    g.fillRect(0, 0, canvas.width, canvas.height);
+    g.fillRect(0, 0, canvas.width, canvas.height)
   }
-});
+})
